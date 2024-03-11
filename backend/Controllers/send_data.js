d21c15fe0302470515df632d0fc109cedb7caee7
Muraliundela <muraliundela369@gmail.com>
2024-03-12 00:11:59 +0530

    Done

--- backend/Controllers/send_data.js
+++ backend/Controllers/send_data.js
@@ -5,7 +5,8 @@ exports.send_data=async(req, res)=>{
         const type=req.params.type;
         console.log(type);
         data=await Recipies.find({type:type});
-        res.status(200).send({data:data,status:true,msg:"Done"});
+        console.log(data);
+        res.status(200).send({data:data,size:data.length,status:true,msg:"Done"});
 
     }catch(err){
         res.status(501).send({status:false,Error:err.message});
