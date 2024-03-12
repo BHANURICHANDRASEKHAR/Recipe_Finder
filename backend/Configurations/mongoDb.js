2cc3f55fee1acc873d1072d18ef3cf90d4f02705
Muraliundela <muraliundela369@gmail.com>
2024-03-12 12:27:06 +0530

    find_by_id is added

--- backend/Configurations/mongoDb.js
+++ backend/Configurations/mongoDb.js
@@ -2,10 +2,9 @@ const mongoose = require('mongoose');
 
 const MongooDB=()=>{
     try{
-       mongoose.connect('mongodb://localhost:27017/test')
+       mongoose.connect('mongodb://localhost:27017/undela')
         .then(()=>{console.log("MongoDb connected")});
-        
-
+    
     }
     catch(err){
         console.log(err);
